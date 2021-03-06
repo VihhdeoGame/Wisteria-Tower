8d12953bd8b3812b63e2ba584a6fdcfb5d22eccf
Victor <52900738+VihhdeoGame@users.noreply.github.com>
2021-03-06 14:08:03 -0300

    Basic Movement Using Templates
    Added a basic movement script with animations using templete assets

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    private Rigidbody2D rb;
+    private Vector2 move;
+    private Animator animator;
+
+    private void Start() 
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", move.x);
+        animator.SetFloat("Vertical", move.y);
+        animator.SetFloat("Speed", move.sqrMagnitude);
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);        
+    }
+}
